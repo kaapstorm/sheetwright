f99940f0aea531cfac557ad1afacece2265a864d
Norman Hooper <nhooper@dimagi.com>
2026-05-01 02:22:29 -0400

    calc: scaffold calc + testing packages, add testsweet dep

---
+++ src/claudesheets/calc/__init__.py
@@ -0,0 +1,5 @@
+"""Calc engine plugin layer.
+
+A calc engine evaluates a built `.xlsx` and returns the calculated
+values for every cell. The default engine is LibreOffice headless.
+"""
---
+++ src/claudesheets/testing/__init__.py
@@ -0,0 +1,1 @@
+"""Test-time helpers exposed to user testsweet tests."""
